319854e09e69e9f69e9d88841126eda0d67771f8
Jasurbek Abdibayev <99121169+abd1bayev@users.noreply.github.com>
2025-01-08 20:45:44 -0800

    Create toq_sonlar_yigindisi.cpp

---
+++ toq_sonlar_yigindisi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    int n, S=0;
+    std::cout << "n sonini kiriting = " << std::endl;
+    cin >> n;
+    std::cout << n << " - sonigacha bo'lgan toq sonlar yigindisi" << std::endl;
+    for (int i = 1; i <= n; i = i + 2) // 1 dan boshlanadi, 2 qadamda oshiriladi
+        S = S+i;
+        std::cout << S << std::endl;
+
+
+
+}
