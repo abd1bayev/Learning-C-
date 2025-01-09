5db37635c66d4587f3a7bcead14878fdebe6c4c8
abd1bayev <jasurabdibayev0@gmail.com>
2025-01-09 21:42:39 +0500

    As

--- main/project/dars_jarayon/toq_sonlar_yigindisi.cpp
+++ main/project/dars_jarayon/toq_sonlar_yigindisi.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 
 using namespace std;
@@ -9,9 +10,13 @@ int main() {
     std::cout << "n sonini kiriting = " << std::endl;
     cin >> n;
     std::cout << n << " - sonigacha bo'lgan toq sonlar yigindisi" << std::endl;
-    for (int i = 1; i <= n; i = i + 2) // 1 dan boshlanadi, 2 qadamda oshiriladi
+    for (int i = 1; i <= n; i = i + 2) { // 1 dan boshlanadi, 2 qadamda oshiriladi
         S = S+i;
-        std::cout << S << std::endl;
+
+
+        
+    }
+    std::cout << S << std::endl;
 
 
 
