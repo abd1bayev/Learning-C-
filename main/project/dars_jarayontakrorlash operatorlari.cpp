59b87cf3638d85d118239b357f4978f9c4b48286
Jasurbek Abdibayev <99121169+abd1bayev@users.noreply.github.com>
2025-01-09 20:33:03 -0800

    Create dars_jarayontakrorlash operatorlari.cpp

---
+++ main/project/dars_jarayontakrorlash operatorlari.cpp
@@ -0,0 +1,19 @@
+// while takrorlash operatorlari
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    int a = 10;
+    std::cout << "while takrorlash operatorlaridan foydalandik" << std::endl;
+    while (a <= 20 )
+    {
+        cout << a << endl;
+        a++;
+    }
+
+    return 0;
+
+}
