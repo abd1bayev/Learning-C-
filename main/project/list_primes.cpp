2c9a20f8d4323bcb5c66e45038fafb06b85726e8
Jasurbek Abdibayev <99121169+abd1bayev@users.noreply.github.com>
2025-01-14 22:41:07 -0800

    Update list_primes.cpp

--- main/project/list_primes.cpp
+++ main/project/list_primes.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// Tub sonni tekshiruvchi funksiya
-bool isPrime(int number) {
-    if (number < 2) return false; // 2 dan kichik sonlar tub emas
-    for (int i = 2; i * i <= number; i++) { // Faqat ildizigacha tekshirish
-        if (number % i == 0) return false;
-    }
-    return true;
-}
-
 int main() {
     int n;
     cout << "n ni kiriting (n > 2): ";
@@ -22,7 +13,15 @@ int main() {
 
     cout << "[2, " << n << "] oraligidagi tub sonlar: ";
     for (int i = 2; i <= n; i++) {
-        if (isPrime(i)) {
+        // Tub sonni tekshirish
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
             cout << i << " ";
         }
     }
@@ -30,6 +29,7 @@ int main() {
 
     return 0;
 }
+
 
 
 /* Foydalanish:
