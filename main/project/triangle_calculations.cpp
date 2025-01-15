9e2677d3f68750986cd913ae1ae65e1e834180a5
Jasurbek Abdibayev <99121169+abd1bayev@users.noreply.github.com>
2025-01-14 23:08:33 -0800

    Update triangle_calculations.cpp

--- main/project/triangle_calculations.cpp
+++ main/project/triangle_calculations.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 int main() {
     // Ikki tomonni va ular orasidagi burchakni kiriting
-    double a, b, angle;
+    double a, b, g;
     cout << "1-tomonni kiriting (a): ";
     cin >> a;
     cout << "2-tomonni kiriting (b): ";
     cin >> b;
     cout << "Ular orasidagi burchakni kiriting (gradusda): ";
-    cin >> angle;
+    cin >> g;
 
     // Burchakni gradusdan radianlarga o'tkazish
-    double angleInRadians = angle * M_PI / 180.0;
+    double rad = g * M_PI / 180.0;
 
     // Kosinus teoremasi yordamida uchinchi tomonni hisoblash
-    double c = sqrt(a * a + b * b - 2 * a * b * cos(angleInRadians));
+    double c = sqrt(a * a + b * b - 2 * a * b * cos(rad));
 
     // Yuzani hisoblash (1/2 * a * b * sin(burchak))
-    double area = 0.5 * a * b * sin(angleInRadians);
+    double area = 0.5 * a * b * sin(rad);
 
     // Natijalarni chiqarish
     cout << "Uchburchakning uchinchi tomoni (c): " << c << endl;
@@ -31,9 +31,23 @@ int main() {
 
 /* Izoh:
 
+    Uchburchakning uchinchi tomon uzunligini va yuzasini hisoblash uchun yozilgan. 
+    Bunda kosinus teoremasi va trigonometrik formulalarni qo‘lladim.
+    
+    
 Foydalanish:
-Dasturga ikki tomon uzunliklarini va ular orasidagi burchakni kiriting.
-Natijada uchinchi tomon va uchburchak yuzasi ekranda chiqadi.
-
+    
+    Dasturga ikki tomon uzunliklarini va ular orasidagi burchakni kiriting.
+    Natijada uchinchi tomon va uchburchak yuzasi ekranda chiqadi.
+    
+    1-tomonni kiriting (a): 5
+    2-tomonni kiriting (b): 7
+    
+    Ular orasidagi burchakni kiriting (gradusda): 60
+    
+    Uchburchakning uchinchi tomoni (c): 6.244
+    Uchburchakning yuzasi: 15.155
+    
+    
 
  */
