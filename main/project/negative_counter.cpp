9ec0b1bee98f98d700b3357e0a91667c8cdb3816
Jasurbek Abdibayev <99121169+abd1bayev@users.noreply.github.com>
2025-01-14 22:28:39 -0800

    Create negative_counter.cpp

---
+++ main/project/negative_counter.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "n ni kiriting (n > 0): ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "n musbat bo'lishi kerak!" << endl;
+        return 1;
+    }
+
+    double number;
+    int negativeCount = 0;
+
+    cout << n << " ta haqiqiy sonni kiriting:" << endl;
+    for (int i = 0; i < n; i++) {
+        cin >> number;
+        if (number < 0) {
+            negativeCount++;
+        }
+    }
+
+    cout << "Manfiy sonlar soni: " << negativeCount << endl;
+
+    return 0;
+}
