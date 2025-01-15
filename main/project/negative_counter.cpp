560594dbdf4f61951b077a25d8994457120d502c
Jasurbek Abdibayev <99121169+abd1bayev@users.noreply.github.com>
2025-01-14 22:58:59 -0800

    Update negative_counter.cpp

--- main/project/negative_counter.cpp
+++ main/project/negative_counter.cpp
@@ -12,17 +12,17 @@ int main() {
     }
 
     double number;
-    int negativeCount = 0;
+    int manfiy_count = 0;
 
     cout << n << " ta haqiqiy sonni kiriting:" << endl;
     for (int i = 0; i < n; i++) {
         cin >> number;
         if (number < 0) {
-            negativeCount++;
+            manfiy_count++;
         }
     }
 
-    cout << "Manfiy sonlar soni: " << negativeCount << endl;
+    cout << "Manfiy sonlar soni: " << manfiy_count << endl;
 
     return 0;
 }
@@ -33,5 +33,10 @@ int main() {
     Dastur ishga tushirilganda n ni kiritasiz.
     Keyin n ta haqiqiy sonni ketma-ket kiriting.
     Natijada manfiy sonlarning soni ekranga chiqariladi.
+    
+    n ni kiriting (n > 0): 5
+    5 ta haqiqiy sonni kiriting:
+        -6 3 -1 2 4.3
+    Manfiy sonlar soni: 2
 
 */
